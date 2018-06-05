6933b4122ef71a3c08056cdb5d96a96b2ccacbc2
Gitea <gitea@fake.local>
2018-06-05 19:39:41 +0800

    mybatis

--- src/main/java/com/wuyoumeiyu/codegenerator/project/mybatis/MyBatisModuleBuilder.java
+++ src/main/java/com/wuyoumeiyu/codegenerator/project/mybatis/MyBatisModuleBuilder.java
@@ -2,21 +2,20 @@ package com.wuyoumeiyu.codegenerator.project.mybatis;
 
 import com.wuyoumeiyu.codegenerator.project.DefaultModuleBuilder;
 import com.wuyoumeiyu.codegenerator.project.Module;
-import com.wuyoumeiyu.codegenerator.project.ModuleBuilder;
 
 public class MyBatisModuleBuilder extends DefaultModuleBuilder {
 
 	public static void main(String args[]) {
 		MybatisProject project=new MybatisProject();
-		project.setProjectName("c:/work/data/test");
+		project.setProjectName("booksharing");
 		
 		Module m=new Module();
 		m.setProject(project);
 		m.setModuleSpecFileName("c:/work/data/test/lxmy.zip");
-		m.set("entity", "rfid");
-		m.set("model", "Rfid");
-		m.set("module", "rfid");
-		m.set("folder", "c:/work/data/test/");
+		m.set("entity", "shop");
+		m.set("model", "Shop");
+		m.set("module", "shop");
+		m.set("folder", "c:/work/Workspace/JEE/booksharing");
 		MyBatisModuleBuilder builder=new MyBatisModuleBuilder();
 		builder.buildModule(m);
 	}
