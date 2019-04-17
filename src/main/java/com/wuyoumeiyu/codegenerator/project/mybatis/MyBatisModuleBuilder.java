19de89158faa317d1003403a6dec1199ea583890
zhouchangjin <iamzhouchangjin@163.com>
2019-04-17 20:55:02 +0800

    变更

--- src/main/java/com/wuyoumeiyu/codegenerator/project/mybatis/MyBatisModuleBuilder.java
+++ src/main/java/com/wuyoumeiyu/codegenerator/project/mybatis/MyBatisModuleBuilder.java
@@ -12,12 +12,12 @@ public class MyBatisModuleBuilder extends DefaultModuleBuilder {
 		Module m=new Module();
 		m.setProject(project);
 		m.setModuleSpecFileName("c:/work/data/test/lxmy.zip");
-		m.set("entity", "userGzh");
-		m.set("model", "UserGzh");
-		m.set("module", "user");
+		m.set("entity", "recycleRecord");
+		m.set("model", "RecycleRecord");
+		m.set("module", "recycle");
 		m.set("folder", "c:/work/Workspace/JEE/booksharing");
-		m.set("path", "user_gzh");
-		m.set("table", "user_gzh");
+		m.set("path", "recycle_record");
+		m.set("table", "recycle_record");
 		MyBatisModuleBuilder builder=new MyBatisModuleBuilder();
 		builder.buildModule(m);
 	}
